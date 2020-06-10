b0d46cabbfb37d9922ef90dfd582b491a00970a9
alex <alex@Mac-mini-alex.local>
2020-06-10 17:14:17 +0300

    yandex fix

--- js/align-yandex.js
+++ js/align-yandex.js
@@ -1,13 +1,17 @@
+// 1.Проверяем экран
+// 2.Ставим плашку 
+// 3.Запускаем изменения и убераем плашку 
+
 function run() {
-    if (document.querySelector('.search2_buttons-outside_2') == null) {
+    if (document.querySelector('.main') == null) {
         setTimeout(() => {
             return run()
         }, 10);
     } else {
         
-        document.querySelector('.main__center').style.marginLeft = '30%'   // плашка поиска
+        document.querySelector('.main').style.marginLeft = '30%'   // плашка поиска
         document.querySelector('.search2_buttons-outside_2').style.marginLeft = '34%' // поиск
-        document.querySelector('.navigation .navigation__region').style.marginLeft = '26%'  // под поиск строка
+        document.querySelector('.navigation .navigation__region').style.marginLeft = '30%'  // под поиск строка
         document.querySelector('#white').remove()
     }
 }
@@ -29,7 +33,7 @@ function preload() {
             newDiv.style.width = "100%";
             newDiv.style.height = "100%";
             newDiv.style.top = "0";
-        
+        // Добавление белого экрана загрузки, пока меняется лейаут
         document.querySelector('body').append(newDiv)
         run()
     }
